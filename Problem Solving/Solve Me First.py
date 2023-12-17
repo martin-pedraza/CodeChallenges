49ba94e5388b0e19d0a2c9d1e4338dc38444650a
Martin Pedraza <56758686+martin-pedraza@users.noreply.github.com>
2023-12-17 15:45:19 -0300

    Add files via upload

---
+++ Problem Solving/Solve Me First.py
@@ -0,0 +1,13 @@
+#Complete the function solveMeFirst to compute the sum of two integers.
+#Example
+#a=7
+#b=3
+#Return 10.
+
+def solveMeFirst(a, b):
+    return a + b
+
+num1 = int(input())
+num2 = int(input())
+res = solveMeFirst(num1, num2)
+print(res)
