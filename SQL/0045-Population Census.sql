b71d5c26e74476b525948081a3fe0829e05c757f
Martin Pedraza <56758686+martin-pedraza@users.noreply.github.com>
2024-02-02 08:35:44 -0300

    Add files via upload

---
+++ SQL/0045-Population Census.sql
@@ -0,0 +1,9 @@
+/*
+
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+*/
+SELECT SUM(CITY.POPULATION)
+FROM CITY
+INNER JOIN COUNTRY
+ON CITY.COUNTRYCODE = COUNTRY.CODE
+WHERE CONTINENT = "Asia"
