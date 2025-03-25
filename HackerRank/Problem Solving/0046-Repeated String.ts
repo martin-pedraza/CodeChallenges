2b6e7ef8752df5b51168a17ed31b1d39f329b6af
Martin Pedraza <56758686+martin-pedraza@users.noreply.github.com>
2025-03-25 09:41:24 -0300

    Update 0046-Repeated String.ts

--- HackerRank/Problem Solving/0046-Repeated String.ts
+++ HackerRank/Problem Solving/0046-Repeated String.ts
@@ -1,3 +1,5 @@
+//There is a string, s, of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first n letters of the infinite string.
+
 'use strict';
 
 import { WriteStream, createWriteStream } from "fs";
